9196016c7d416057f9c10cf35a0025a296b59bc0
Rainy <geekrainy@gmail.com>
2019-03-12 16:52:10 +0800

    Remove git directory after init

--- packages/commands/init.js
+++ packages/commands/init.js
@@ -5,7 +5,8 @@ function init(dirname) {
   const fetchProcess = spawn('git', ['clone', 'git@github.com:geekrainy/rainywebpack', path.resolve(dirname)], { stdio: 'inherit' });
 
   fetchProcess.on('close', function() {
-    spawn('yarn', ['install', '--cwd', path.resolve(dirname)], { stdio: 'inherit' })
+    spawn('rm', ['-rf', path.resolve(dirname, '.git')]);
+    spawn('yarn', ['install', '--cwd', path.resolve(dirname)], { stdio: 'inherit' });
   })
 
 }
