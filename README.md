7be5d8d380bb2ab219f1aaabc0c791dc747a21c9
Rainy <geekrainy@gmail.com>
2019-03-27 20:33:00 +0800

    Release v0.0.7: Minor bug fixes

--- README.md
+++ README.md
@@ -2,4 +2,44 @@
 
 [![npm](https://img.shields.io/npm/v/rw-cli.svg?style=flat-square)](https://www.npmjs.com/package/rw-cli)  [![npm](https://img.shields.io/npm/dm/rw-cli.svg?style=flat-square)](https://www.npmjs.com/package/rw-cli)  [![npm](https://img.shields.io/npm/dt/rw-cli.svg?style=flat-square)](https://www.npmjs.com/package/rw-cli)  [![npm](https://img.shields.io/npm/l/rw-cli.svg?style=flat-square)](https://www.npmjs.com/package/rw-cli)
 
-[rainywebpack](https://github.com/geekrainy/rainywebpack) 命令行工具。
+CLI for [rainywebpack](https://github.com/geekrainy/rainywebpack).
+
+## Getting Started
+
+```shell
+# Install
+$ yarn global add rw-cli
+
+# Create rainywebpack project
+$ rw new demo
+
+# Start rainywebpack
+$ cd demo
+$ yarn start
+```
+
+## Generated File Tree
+
+```shell
+.
+├── .babelrc
+├── .eslintignore
+├── .eslintrc.js
+├── .gitignore
+├── .prettierignore
+├── .prettierrc
+├── .stylelintrc
+├── README.md
+├── package.json
+├── src
+│   ├── App.js
+│   ├── App.less
+│   ├── avatar.jpg
+│   ├── index.ejs
+│   ├── index.js
+│   └── utils.js
+├── webpack.common.js
+├── webpack.dev.js
+├── webpack.prod.js
+└── yarn.lock
+```
--- packages/utils.js
+++ packages/utils.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const fse = require('fs-extra');
 const chalk = require('chalk');
 const which = require('which');
+const ProgressBar = require('progress');
 
 const execPath = __dirname;
 const corePath = path.resolve(execPath.slice(0, -8), 'rainywebpack');
@@ -12,36 +13,54 @@ const corePath = path.resolve(execPath.slice(0, -8), 'rainywebpack');
 function updateCore(ts) {
   const branch = ts ? 'ts' : 'master';
   return new Promise((resolve, reject) => {
-    const updateProcess = spawn(`cd ${corePath} && git checkout ${branch} && git pull origin ${branch}`, {
-      stdio: 'inherit',
+    const updateBar = new ProgressBar(`${chalk.blue('Updating rainywebpack:')} [:bar] :percent`, {
+      complete: '=',
+      incomplete: '-',
+      total: 30,
+    });
+    const updateProcess = spawn(`git checkout ${branch} && git pull origin ${branch}`, {
       shell: true,
       cwd: corePath,
     })
+    const timer = setInterval(function (){
+      updateBar.tick();
+      if (updateBar.curr === 28) {
+        clearInterval(timer);
+      }
+    }, 200);
 
-    updateProcess.on('exit', () => resolve());
+    updateProcess.on('exit', () => {
+      updateBar.tick(30);
+      clearInterval(timer)
+      resolve();
+    });
     updateProcess.on('error', err => reject(err));
   })
-
 }
 
 function copyCore(destPath) {
   const appName = destPath.split('/').pop();
   const list = fs.readdirSync(corePath).filter(f => !['.DS_Store', '.git', 'node_modules', 'package.json', '*.bak', '*.*.bak', '*.tmp', '*.swp'].includes(f));
-  return new Promise((resolve, reject) => {
-    Promise.all(list.map(file => {
-      return fse.copy(`${corePath}/${file}`, `${destPath}/${file}`)
-        .then(() => console.log(chalk.green(`create: ${destPath}/${file}`)))
-    }))
-      .then(() => {
-        const appPkg = require(`${corePath}/package.json`);
-        appPkg.name = appName;
-        appPkg.description = appName;
-        appPkg.author = '';
 
-        writeFile(`${destPath}/package.json`, JSON.stringify(appPkg, null, 2));
-        resolve();
-      })
-      .catch(err => reject(err));
+  console.log(chalk.green('Initial rainywebpack...'))
+
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      Promise.all(list.map(file => {
+        return fse.copy(`${corePath}/${file}`, `${destPath}/${file}`)
+          .then(() => console.log(chalk.green(`create: ${destPath}/${file}`)))
+      }))
+        .then(() => {
+          const appPkg = require(`${corePath}/package.json`);
+          appPkg.name = appName;
+          appPkg.description = appName;
+          appPkg.author = '';
+  
+          writeFile(`${destPath}/package.json`, JSON.stringify(appPkg, null, 2));
+          resolve();
+        })
+        .catch(err => reject(err));
+    }, 500)
   })
 }
 
