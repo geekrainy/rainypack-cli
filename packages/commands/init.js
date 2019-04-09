e68099e012a965637cc3ece125e265bfa5dfc345
Rainy <geekrainy@gmail.com>
2019-04-09 19:26:31 +0800

    Release v0.1.0: Refactor

--- packages/commands/init.js
+++ packages/commands/init.js
@@ -6,12 +6,12 @@ const chalk = require('chalk');
 const ProgressBar = require('progress');
 
 const { writeFile, findYarn, emptyDirectory, confirm } = require('../utils/utils');
-const { CORE_IGNORE_LIST, CORE_NAME, CORE_TYPE } = require('../utils/constants');
+const { CORE_PATH_PREFIX, CORE_IGNORE_LIST, TS_CORE_IGNORE_LIST, CORE_NAME, CORE_TYPE } = require('../utils/constants');
 
 const packagePath = __dirname.split(path.sep).slice(0, -2).join(path.sep);
 const coreConfig = {
   name: CORE_NAME.webpack,
-  path: path.resolve(packagePath, CORE_NAME.webpack),
+  path: path.resolve(packagePath, 'node_modules', CORE_NAME.webpack),
   type: CORE_TYPE.default,
 }
 
@@ -43,7 +43,7 @@ async function create(dirname, cmd) {
   }
 
   if (rollup) {
-    coreConfig.path = path.resolve(packagePath, CORE_NAME.rollup);
+    coreConfig.path = path.resolve(packagePath, 'node_modules', CORE_NAME.rollup);
   }
 
   await updateCore();
@@ -60,9 +60,8 @@ function updateCore() {
       incomplete: '-',
       total: 30,
     });
-    const updateProcess = spawn(`git checkout ${coreConfig.type} && git pull origin ${coreConfig.type}`, {
-      shell: true,
-      cwd: coreConfig.path,
+    const updateProcess = spawn(`yarn upgrade ${coreConfig.name}`, {
+      shell: true
     })
     const timer = setInterval(function (){
       updateBar.tick();
@@ -82,18 +81,37 @@ function updateCore() {
 
 function copyCore(destPath) {
   const appName = destPath.split(path.sep).pop();
-  const list = fs.readdirSync(coreConfig.path).filter(f => !CORE_IGNORE_LIST.includes(f));
+
+  const defaultCorePath = `${coreConfig.path}/lib`;
+  const tsCorePath = `${coreConfig.path}/ts`;
+
+  const ignoreList = coreConfig.type === 'ts' ? [...CORE_IGNORE_LIST, ...TS_CORE_IGNORE_LIST] : CORE_IGNORE_LIST;
+  const appPkgPath = coreConfig.type === 'ts' ? `${tsCorePath}/package.json` : `${defaultCorePath}/package.json`;
+
+  let list = fs.readdirSync(defaultCorePath)
+    .filter(f => !ignoreList.includes(f))
+    .map(f => `${CORE_PATH_PREFIX.default}/${f}`);
+
+  if (coreConfig.type === 'ts') {
+    const tsList = fs.readdirSync(tsCorePath)
+      .filter(f => !CORE_IGNORE_LIST.map(f => `${CORE_PATH_PREFIX.ts}/${f}`).includes(f))
+      .map(f => `${CORE_PATH_PREFIX.ts}/${f}`);
+
+    list = list.concat(tsList);
+  }
 
   console.log(chalk.green('Initial rainypack...'))
 
   return new Promise((resolve, reject) => {
     setTimeout(function () {
       Promise.all(list.map(file => {
-        return fse.copy(`${coreConfig.path}/${file}`, `${destPath}/${file}`)
-          .then(() => console.log(chalk.green(`create: ${destPath}/${file}`)))
+        const destFileName = file.replace(/(lib|ts)\//, '');
+
+        return fse.copy(`${coreConfig.path}/${file}`, `${destPath}/${destFileName}`)
+          .then(() => console.log(chalk.green(`create: ${destPath}/${destFileName}`)))
       }))
         .then(() => {
-          const appPkg = require(`${coreConfig.path}/package.json`);
+          const appPkg = require(appPkgPath);
           appPkg.name = appName;
           appPkg.description = appName;
           appPkg.author = '';
--- packages/utils/constants.js
+++ packages/utils/constants.js
@@ -1,3 +1,7 @@
+const CORE_PATH_PREFIX = {
+  default: 'lib',
+  ts: 'ts'
+}
 const CORE_IGNORE_LIST = [
   '.DS_Store',
   '.git',
@@ -8,17 +12,25 @@ const CORE_IGNORE_LIST = [
   '*.tmp',
   '*.swp'
 ]
+const TS_CORE_IGNORE_LIST = [
+  'src',
+  '.eslintrc.js',
+  'yarn.lock',
+  'webpack.common.js',
+]
 const CORE_NAME = {
   webpack: 'rainypack-webpack',
   rollup: 'rainypack-rollup'
 }
 const CORE_TYPE = {
-  default: 'master',
+  default: 'default',
   ts: 'ts',
 }
 
 module.exports = {
+  CORE_PATH_PREFIX,
   CORE_IGNORE_LIST,
+  TS_CORE_IGNORE_LIST,
   CORE_NAME,
   CORE_TYPE
 }
